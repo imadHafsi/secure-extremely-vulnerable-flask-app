8b76ce68a9a09a4be1469086eb98d2dc5198f359
imadhafsi <imadhafsi94@gmail.com>
2025-11-30 13:37:37 +0000

    disable SQL logging by default and on/off them using env var

--- models/__init__.py
+++ models/__init__.py
@@ -10,12 +10,16 @@ from .note import Note
 
 DB_URL = os.environ.get("DATABASE_URL", Config.SQLALCHEMY_DATABASE_URI)
 
+#SQL logging on/off without changing code, just by setting an env var.
+#windows =>  $env:SQL_ECHO = "true" / "false"
+
+DEBUG_SQL = os.environ.get("SQL_ECHO", "false").lower() == "true"
+
 engine: Engine = create_engine(
     DB_URL,
-    echo=True,
+    echo=DEBUG_SQL,
 )
 
 BaseModel.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
- 
